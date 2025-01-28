ee2ad2615a9fc5beeb78221bf2ce10720ec76821
Manuj Rai <imanujrai7@gmail.com>
2025-01-28 16:07:29 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.html 	modified:   src/styles.css 	new file:   tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Includes all Angular template and TypeScript files
+  ],
+  theme: {
+    extend: {}, // Add customizations here
+  },
+  plugins: [],
+};
+
