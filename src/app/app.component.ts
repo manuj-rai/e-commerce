32b9bdca4ea8e3a529a6626bc29441a98c215a83
Manuj Rai <imanujrai7@gmail.com>
2025-01-29 15:36:27 +0530

    new file:   src/app/Main-Layout/categories/categories.component.css 	new file:   src/app/Main-Layout/categories/categories.component.html 	new file:   src/app/Main-Layout/categories/categories.component.spec.ts 	new file:   src/app/Main-Layout/categories/categories.component.ts 	modified:   src/app/Main-Layout/top-slider/top-slider.component.html 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { NavbarComponent } from "./Main-Layout/navbar/navbar.component";
 import { TopSliderComponent } from "./Main-Layout/top-slider/top-slider.component";
 import { CarouselComponent } from "./Main-Layout/carousel/carousel.component";
 import { FooterComponent } from "./Main-Layout/footer/footer.component";
+import { CategoriesComponent } from "./Main-Layout/categories/categories.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, TopSliderComponent, CarouselComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, TopSliderComponent, CarouselComponent, FooterComponent, CategoriesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
