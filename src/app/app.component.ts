adc74d1f2e5deba92b7f774180a02e525dba261f
Manuj Rai <imanujrai7@gmail.com>
2025-01-29 12:47:41 +0530

    modified:   src/app/Main-Layout/carousel/carousel.component.html 	new file:   src/app/Main-Layout/footer/footer.component.css 	new file:   src/app/Main-Layout/footer/footer.component.html 	new file:   src/app/Main-Layout/footer/footer.component.spec.ts 	new file:   src/app/Main-Layout/footer/footer.component.ts 	modified:   src/app/Main-Layout/navbar/navbar.component.html 	modified:   src/app/Main-Layout/top-slider/top-slider.component.html 	modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/styles.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./Main-Layout/navbar/navbar.component";
 import { TopSliderComponent } from "./Main-Layout/top-slider/top-slider.component";
 import { CarouselComponent } from "./Main-Layout/carousel/carousel.component";
+import { FooterComponent } from "./Main-Layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, TopSliderComponent, CarouselComponent],
+  imports: [RouterOutlet, NavbarComponent, TopSliderComponent, CarouselComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
