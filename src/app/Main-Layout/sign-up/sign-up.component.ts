5e733779efb5863581c1c11533ec665fe4460de3
Manuj Rai <imanujrai7@gmail.com>
2025-02-03 10:33:10 +0530

    modified:   src/app/Main-Layout/sign-up/sign-up.component.html 	modified:   src/app/Main-Layout/sign-up/sign-up.component.ts

--- src/app/Main-Layout/sign-up/sign-up.component.ts
+++ src/app/Main-Layout/sign-up/sign-up.component.ts
@@ -7,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  imagePreview: string | ArrayBuffer | null = null;
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imagePreview = reader.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 
 }
